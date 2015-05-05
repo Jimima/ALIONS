8330120d619a308d055c8713f62c5c9fcf60e744
Jason Porter <jsnprtr@gmail.com>
2015-05-05 23:14:39 +0200

    Forward momentum now 'works'

--- js/objects/ship.js
+++ js/objects/ship.js
@@ -8,25 +8,30 @@ var Ship = function(){
 	ship.forwardMomentum = 0;
 	ship.angularMomentum = 0;
 
+	ship.angleAsDegrees = function(){
+		return ship.angle * Math.PI / 180;
+	};
+
 	ship.moveRight = function(){
-		ship.angularMomentum = 0.4;
+		ship.angularMomentum = 10;
 	};
 
 	ship.moveLeft = function(){
-		ship.angularMomentum = -0.4;
+		ship.angularMomentum = -10;
 	};
 
 	ship.moveUp = function(){
-		ship.y -= 10;
+		ship.forwardMomentum = 10;
 	};
 
 	ship.moveDown = function(){
-		ship.y += 10;
+		ship.forwardMomentum = -10;
 	};
 
 	ship.updatePosition = function(){
 		ship.angle = ship.angle += ship.angularMomentum;
 		ship.angularMomentum = ship.angularMomentum / 1.2;
+		ship.forwardMomentum = ship.forwardMomentum / 1.1;
 	};
 
 	ship.draw = function(context){
@@ -36,10 +41,14 @@ var Ship = function(){
 		context.lineWidth = 2;
 		context.strokeStyle = "black";
 
+		sin = Math.sin(ship.angle * Math.PI / 180);
+		cos = Math.cos(ship.angle * Math.PI / 180);
+
+		ship.x += cos * ship.forwardMomentum;
+		ship.y += sin * ship.forwardMomentum;
+
 		// draw ship lines
 		for(i = 0; i < ship.shipPathLength; i++){
-			sin = Math.sin(ship.angle);
-			cos = Math.cos(ship.angle);
 			xnew = ship.path[i].x * cos - ship.path[i].y * sin;
 			ynew = ship.path[i].x * sin + ship.path[i].y * cos;
 			context.lineTo(xnew + ship.x, ynew + ship.y);
@@ -49,10 +58,19 @@ var Ship = function(){
 	};
 
 	(function(){
+		var cos, sin, xnew, ynew;
 		ship.path.push({x: 0, y: -50});
 		ship.path.push({x: -30, y: 50});
 		ship.path.push({x: 0, y: 30});
 		ship.path.push({x: 30, y: 50});
+		cos = Math.cos(90 * Math.PI / 180);
+		sin = Math.sin(90 * Math.PI / 180);
 		ship.shipPathLength = ship.path.length;
+		for(var i = 0; i < ship.shipPathLength; i++){
+			xnew = ship.path[i].x * cos - ship.path[i].y * sin;
+			ynew = ship.path[i].x * sin + ship.path[i].y * cos;
+			ship.path[i].x = xnew;
+			ship.path[i].y = ynew;
+		}
 	})();
 };
